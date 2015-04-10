8ed2c7a28b1f1b0be0ab00e471b1157a0aea71ba
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-09 23:03:03 -0700

    working on adding pieces

--- client/index.js
+++ client/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+$(document).ready(init);
+
+function init(){
+
+}
+
+var green = "url('/asset/piece_green.jpg')";
+
+function putPieceOn(x, y, color){
+  if (color === 'blue'){
+  $('tr:nth-child(' + y + ') td:nth-child(' + x + ')').addClass('addBluePiece');
+  }
+  else {$('tr:nth-child(' + y + ') td:nth-child(' + x + ')').addClass('addGreenPiece');}
+}
