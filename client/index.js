889ee343a339cf03d05bb4fed9ff96923a1ffe3a
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-10 21:48:15 -0700

    fix resetting board

--- client/index.js
+++ client/index.js
@@ -11,24 +11,124 @@ var newBoard = {21:'blue', 41:'blue', 61:'blue', 81:'blue',
                 27:'green', 47:'green', 67:'green', 87:'green',
                 18:'green', 38:'green', 58:'green', 78:'green',};
 var currentBoard = newBoard;
+var current = 'soccer';
+var $source;
+var flashAction;
 
 function init(){
-  $('#start').click(fillBoard);
-  $('#reset').click(fillBoard);
+  initBoard();
+  $('#start').click(startGame);
+  $('#reset').click(initBoard);
+  $('#board').on('click', '.active', select);
+  $('#board').on('click', '.empty', move);
 }
 
-// var green = "url('/asset/piece_green.jpg')";
+// function putPieceOn(num, color){
+//   var x = num.toString()[0];
+//   var y = num.toString()[1];
+//   if (color === 'blue'){
+//   $('tr:nth-child(' + y + ') td:nth-child(' + x + ')').addClass('addBluePiece');
+//   }
+//   else {$('tr:nth-child(' + y + ') td:nth-child(' + x + ')').addClass('addGreenPiece');}
+// }
+// function fillBoard(){
+//   for (var key in newBoard){
+//     if (newBoard[key] !== 'empty') {putPieceOn(key, newBoard[key]);}
+//   }
+// }
 
-function putPieceOn(num, color){
-  var x = num.toString()[0];
-  var y = num.toString()[1];
-  if (color === 'blue'){
-  $('tr:nth-child(' + y + ') td:nth-child(' + x + ')').addClass('addBluePiece');
-  }
-  else {$('tr:nth-child(' + y + ') td:nth-child(' + x + ')').addClass('addGreenPiece');}
+function initBoard(){
+  $('.valid').removeClass('soccer football player active empty');
+  $('#board tr:lt(3) .valid').addClass('soccer player');
+  $('#board tr:gt(4) .valid').addClass('football player');
+  $('.valid:not(.player)').addClass('empty');
+  // $('.valid ')
+  flashBoard();
 }
-function fillBoard(){
-  for (var key in newBoard){
-    if (newBoard[key] !== 'empty') {putPieceOn(key, newBoard[key]);}
+
+function startGame(){
+  stopFlash();
+}
+
+function switchUser(){
+  current = (current === 'football') ? 'soccer' : 'football';
+  $('.valid').removeClass('active selected');
+  $('.' + current).addClass('active');
+}
+
+function flashBoard(){
+  flashAction = setInterval(switchUser, 300);
+}
+
+function stopFlash(){
+  clearInterval(flashAction);
+}
+
+function move(){
+  if(!$source){
+    return;
   }
+  var $target = $(this);
+  //isKing = $source.hasClass('king'); //we can do this
+  var isKing = $source.is('.king');
+
+  var src = {};
+  var tgt = {};
+
+  src.x = $source.data('x') * 1;
+  src.y = $source.data('y') * 1;
+  tgt.x = $target.data('x') * 1;
+  tgt.y = $target.data('y') * 1;
+
+  var compass = {};
+  compass.north = (current === 'football') ? -1 : 1;
+  compass.east = (current==='football') ? 1 : -1;
+  compass.west = compass.east * -1;
+  compass.south = compass.north * - 1;
+
+  switch (moveType(src, tgt, compass, isKing)){
+    case 'move':
+      console.log('Its a move');
+      movePiece($source, $target);
+      switchUser();
+      break;
+    case'jump':
+      console.log('Its a jump');
+  }
+}
+
+function movePiece($source, $target){
+  var targetClasses = $target.attr('class');  //all classes on target
+  var sourceClasses = $source.attr('class');
+  $target.attr('class', sourceClasses);
+  $source.attr('class', targetClasses);
+}
+
+function moveType(src, tgt, compass, isKing){
+  if (isMove (src, tgt, compass, isKing)){
+    return 'move';
+  }
+  if (isJump() && isEnemy()){
+    return 'jump';
+  }
+}
+
+function isMove(src, tgt, compass, isKing){
+  return (src.x + compass.east === tgt.x || src.x + compass.west === tgt.x) &&
+    (src.y + compass.north === tgt.y || (isKing && src.y + compass.south === tgt.y));
+}
+
+function isJump(){
+
+}
+
+function isEnemy(){
+
+}
+
+function select(){
+  $source = $(this);  //--> normal td to jquery td
+  $('.valid').removeClass('selected');
+  $source.addClass('selected');
+
 }
